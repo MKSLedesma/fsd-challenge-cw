caf8be007aa6091962ea9e994e7018ad7d2bf359
MKSLedesma <cledesma@fi.uba.ar>
2026-09-18 20:50:16 -0300

    add auth endpoints to the app

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const authRoutes = require('./routes/authRoutes');
 
 const app = express();
 const PORT = process.env.PORT || 4000;
@@ -10,6 +11,8 @@ app.use(express.json());
 app.get('/api/health', (req, res) => {
     res.json({status: 'ok'});
 });
+
+app.use('/api/auth', authRoutes);
 
 app.listen(PORT, () => {
     console.log(`Servidor corriendo en http://localhost:${PORT}`)
