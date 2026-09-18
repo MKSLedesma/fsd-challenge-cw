cce1b3547533b6f1e152376fd15bfa8a64a23778
MKSLedesma <cledesma@fi.uba.ar>
2026-09-18 19:45:53 -0300

    add utils functions for persisted data
    add async functions for read users and write users for optimize io processes

---
+++ backend/src/utils/db.js
@@ -0,0 +1,27 @@
+const { write } = require('fs');
+const fs = require('fs/promises');
+const path = require('path');
+
+const userFilePath = path.join(__dirname, '../../database/users.json');
+
+async function readUsers() {
+    try {
+        const data = await fs.readFile(userFilePath, 'utf-8');
+        return JSON.parse(data || '[]');
+    } catch (error) {
+        if (error.code === 'ENOENT') {
+            await fs.writeFile(userFilePath, '[]');
+            return [];
+        }
+        throw error;
+    }
+}
+
+async function writeUsers(users) {
+    await fs.writeFile(usersFilePath, JSON.stringify(users, null, 2), 'utf-8');
+}
+
+module.exports = {
+    readUsers,
+    writeUsers
+};
