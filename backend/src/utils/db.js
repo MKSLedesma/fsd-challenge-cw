8b47cc4e935b8eac8a074533252eae45d8017bdc
MKSLedesma <crisledmess@gmail.com>
2026-09-18 22:51:43 -0300

    add read animals json function (todo: modularize)

--- backend/src/utils/db.js
+++ backend/src/utils/db.js
@@ -2,6 +2,7 @@ const fs = require('fs/promises');
 const path = require('path');
 
 const userFilePath = path.join(__dirname, '../../database/users.json');
+const animalsFilePath = path.join(__dirname, '../../database/animals.json');
 
 async function readUsers() {
     try {
@@ -20,7 +21,21 @@ async function writeUsers(users) {
     await fs.writeFile(userFilePath, JSON.stringify(users, null, 2), 'utf-8');
 }
 
+async function readAnimals() {
+    try {
+        const data = await fs.readFile(animalsFilePath, 'utf-8');
+        return JSON.parse(data || '[]');
+    } catch (error) {
+        if (error.code === 'ENOENT') {
+            await fs.writeFile(animalsFilePath, '[]');
+            return [];
+        }
+        throw error;
+    }
+}
+
 module.exports = {
     readUsers,
-    writeUsers
+    writeUsers,
+    readAnimals
 };
