3a3539bb7cfff82834fd3063c9375e957b774c92
MKSLedesma <crisledmess@gmail.com>
2026-09-21 17:53:44 -0300

    separate express app configuration from server start. Also add node test script

---
+++ backend/src/app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const cors = require('cors');
+const authRoutes = require('./routes/authRoutes');
+const animalRoutes = require('./routes/animalRoutes');
+const authenticateToken = require('./middleware/authMiddleware');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use('/api/auth', authRoutes);
+app.use('/api/animales', authenticateToken, animalRoutes);
+
+module.exports = app;
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,17 +1,5 @@
-const express = require('express');
-const cors = require('cors');
-const authRoutes = require('./routes/authRoutes');
-const animalRoutes = require('./routes/animalRoutes');
-const authenticateToken = require('./middleware/authMiddleware');
-
-const app = express();
+const app = require('./app');
 const PORT = process.env.PORT || 4000;
-
-app.use(cors());
-app.use(express.json());
-
-app.use('/api/auth', authRoutes);
-app.use('/api/animales', authenticateToken, animalRoutes);
 
 app.listen(PORT, () => {
     console.log(`Servidor corriendo en http://localhost:${PORT}`)
