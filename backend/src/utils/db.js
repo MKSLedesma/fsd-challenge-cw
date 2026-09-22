68fbdd91296881d744f2937630597c0e1a2dff6d
MKSLedesma <crisledmess@gmail.com>
2026-09-21 22:35:05 -0300

    remove unused helpers

--- backend/src/utils/db.js
+++ backend/src/utils/db.js
@@ -1,25 +1,7 @@
 const fs = require('fs/promises');
 const path = require('path');
 
-const userFilePath = path.join(__dirname, '../../database/users.json');
 const animalsFilePath = path.join(__dirname, '../../database/animals.json');
-
-async function readUsers() {
-    try {
-        const data = await fs.readFile(userFilePath, 'utf-8');
-        return JSON.parse(data || '[]');
-    } catch (error) {
-        if (error.code === 'ENOENT') {
-            await fs.writeFile(userFilePath, '[]');
-            return [];
-        }
-        throw error;
-    }
-}
-
-async function writeUsers(users) {
-    await fs.writeFile(userFilePath, JSON.stringify(users, null, 2), 'utf-8');
-}
 
 async function readAnimals() {
     try {
@@ -35,7 +17,5 @@ async function readAnimals() {
 }
 
 module.exports = {
-    readUsers,
-    writeUsers,
     readAnimals
 };
